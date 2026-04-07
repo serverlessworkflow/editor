08f7b31ea5d1fe9470f9d95700d3c8663dcc0b02
kumaradityaraj <sedulous.0007@gmail.com>
2026-04-07 18:32:19 +0530

    copilot suggestion
    Signed-off-by: kumaradityaraj <sedulous.0007@gmail.com>

--- packages/i18n/vitest.config.ts
+++ packages/i18n/vitest.config.ts
@@ -20,7 +20,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     tsconfigPaths({
-      projects: ["./tsconfig.test.json"],
+      projects: ["./tsconfig.json"],
     }),
   ],
   test: {
