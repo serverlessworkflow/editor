2f7a83a22aaacc2f54a975020ffb9606fe8ab1a5
fantonangeli <fabrizio.antonangeli@gmail.com>
2026-04-14 19:46:21 +0200

    Removed vite-tsconfig-paths
    Signed-off-by: fantonangeli <fabrizio.antonangeli@gmail.com>

--- packages/i18n/vitest.config.ts
+++ packages/i18n/vitest.config.ts
@@ -15,14 +15,11 @@
  */
 
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     globals: true,
     environment: "jsdom",
